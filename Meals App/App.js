b21e96a8d603c9c97b460759525c686dd7722913
Yukihide Takahashi <yukihide99@gmail.com>
2022-12-04 19:31:51 +0900

    installing navigation packages

--- Meals App/App.js
+++ Meals App/App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import CategoriesScreen from "./screens/CategoriesScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCatagories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }

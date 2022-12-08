03bd8fef91e0a81cff94d39489ad059a573fd532
Yukihide Takahashi <yukihide99@gmail.com>
2022-12-08 14:21:47 +0900

    displaying meals

--- Meals App/screens/MealsOverviewScreen.js
+++ Meals App/screens/MealsOverviewScreen.js
@@ -1,10 +1,24 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
+import MealItem from "../components/MealItem";
 import { MEALS } from "../data/dummy-data";
 
-function MealsOverviewScreen() {
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealIteam(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
   return (
     <View style={styles.container}>
-      <Text>Meals Overview</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealIteam}
+      />
     </View>
   );
 }

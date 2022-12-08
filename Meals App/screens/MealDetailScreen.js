957ff12a5711e9b49acf646142b0e71160803237
Yukihide Takahashi <yukihide99@gmail.com>
2022-12-08 21:26:14 +0900

    adding content to Meal Detail screen

--- Meals App/screens/MealDetailScreen.js
+++ Meals App/screens/MealDetailScreen.js
@@ -1,8 +1,34 @@
-import { Text } from "react-native";
+import { Text, View, Image } from "react-native";
+import MealDetails from "../components/MealDetails";
+
+import { MEALS } from "../data/dummy-data";
 
 function MealDetailScreen({ route, navigation }) {
   const mealId = route.params.mealId;
-  return <Text>This is the Meal Detail screen ({mealId})</Text>;
+
+  const selectedMeal = MEALS.find((meal) => {
+    return meal.id === mealId;
+  });
+
+  return (
+    <View>
+      <Image source={{ uri: selectedMeal.imageUrl }} />
+      <Text>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+      />
+      <Text>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient) => (
+        <Text key={ingredient}>{ingredient}</Text>
+      ))}
+      <Text>Steps</Text>
+      {selectedMeal.steps.map((step) => (
+        <Text key={step}>{step}</Text>
+      ))}
+    </View>
+  );
 }
 
 export default MealDetailScreen;

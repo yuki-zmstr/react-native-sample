2e341adbd909b7bc9e5ea70def615aef66e43fa3
yuki-zmstr <yukihide99@gmail.com>
2022-12-12 16:00:00 +0900

    modifications to other navigators project

--- Other Navigators/App.js
+++ Other Navigators/App.js
@@ -1,18 +1,5 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-
-import WelcomeScreen from "./screens/WelcomeScreen";
-import UserScreen from "./screens/UserScreen";
-
-const Drawer = createDrawerNavigator();
+import { Text } from 'react-native';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="User">
-        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
-        <Drawer.Screen name="User" component={UserScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+  return <Text>Todo...</Text>;
 }

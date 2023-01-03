e16add0b45796958e1e63d9e541741cded15024c
yuki-zmstr <yukihide99@gmail.com>
2023-01-03 10:52:37 +0800

    sending get request

--- Http requests/screens/RecentExpenses.js
+++ Http requests/screens/RecentExpenses.js
@@ -1,11 +1,19 @@
-import { useContext } from 'react';
+import { useEffect, useContext } from 'react';
 
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 import { ExpensesContext } from '../store/expenses-context';
 import { getDateMinusDays } from '../util/date';
+import { fetchExpenses } from '../util/http';
 
 function RecentExpenses() {
   const expensesCtx = useContext(ExpensesContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpenses();
+    }
+    getExpenses();
+  }, []);
 
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
@@ -17,8 +25,8 @@ function RecentExpenses() {
   return (
     <ExpensesOutput
       expenses={recentExpenses}
-      expensesPeriod="Last 7 Days"
-      fallbackText="No expenses registered for the last 7 days."
+      expensesPeriod='Last 7 Days'
+      fallbackText='No expenses registered for the last 7 days.'
     />
   );
 }
--- Http requests/util/http.js
+++ Http requests/util/http.js
@@ -1,8 +1,24 @@
 import axios from 'axios';
 
+const BACKEND_URL =
+  'https://react-native-project-7ac4f-default-rtdb.asia-southeast1.firebasedatabase.app';
+
 export function storeExpense(expenseData) {
-  axios.post(
-    'https://react-native-project-7ac4f-default-rtdb.asia-southeast1.firebasedatabase.app/expenses.json',
-    expenseData,
-  );
+  axios.post(BACKEND_URL + '/expenses.json', expenseData);
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + '/expenses.json');
+
+  const expenses = [];
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].descripton,
+    };
+    expenses.push(expenseObj);
+  }
+  return expenses;
 }

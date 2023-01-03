b0ced9240f6a2d9405af3fa0c8e7d3358cf0ddd7
yuki-zmstr <yukihide99@gmail.com>
2023-01-03 09:10:07 +0800

    managing form state

--- User Input/components/ManageExpense/ExpenseForm.jsx
+++ User Input/components/ManageExpense/ExpenseForm.jsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
 import Input from './Input';
+import Button from '../UI/Button';
 
-const ExpenseForm = () => {
+const ExpenseForm = ({ submitButtonLabel, onCancel, onSubmit }) => {
   const [inputValues, setInputValues] = useState({
     amount: '',
     date: '',
@@ -18,6 +19,8 @@ const ExpenseForm = () => {
       };
     });
   }
+
+  function submitHandler() {}
 
   return (
     <View style={styles.form}>
@@ -53,6 +56,14 @@ const ExpenseForm = () => {
           value: inputValues.description,
         }}
       />
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode='flat' onPress={onCancel}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={submitHandler}>
+          {submitButtonLabel}
+        </Button>
+      </View>
     </View>
   );
 };
@@ -76,5 +87,14 @@ const styles = StyleSheet.create({
   },
   rowInput: {
     flex: 1,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
 });

e665e466f0655882ceb69bc89ace4d5b8082c810
yuki-zmstr <yukihide99@gmail.com>
2023-01-04 08:51:36 +0800

    adding util function for posting

---
+++ Auth/util/auth.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API_KEY = 'AIzaSyAqip2KDGtuDz4YRUL5e40PRuIQuX3dc3A';
+
+async function createUser(email, password) {
+  const response = await axios.post(
+    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    },
+  );
+}

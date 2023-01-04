4133b948ab3ff459bff9c1b108569543cb4f58cf
yuki-zmstr <yukihide99@gmail.com>
2023-01-04 09:06:07 +0800

    logging users in

--- Auth/util/auth.js
+++ Auth/util/auth.js
@@ -2,13 +2,26 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyAqip2KDGtuDz4YRUL5e40PRuIQuX3dc3A';
 
-export async function createUser(email, password) {
+async function authenticate(mode, email, password) {
   const response = await axios.post(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
+    'https://identitytoolkit.googleapis.com/v1/accounts:' +
+      mode +
+      '?key=' +
+      API_KEY,
     {
       email: email,
       password: password,
       returnSecureToken: true,
     },
   );
+
+  console.log(response.data);
+}
+
+export async function createUser(email, password) {
+  await authenticate('signUp', email, password);
+}
+
+export async function login(email, password) {
+  await authenticate('signInWithPassword', email, password);
 }

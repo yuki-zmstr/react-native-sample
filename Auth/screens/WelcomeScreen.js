466768f964f1e5cab581a0a81cd32284f0de545b
yuki-zmstr <yukihide99@gmail.com>
2023-01-04 10:47:27 +0800

    accessing proected resources

--- Auth/screens/WelcomeScreen.js
+++ Auth/screens/WelcomeScreen.js
@@ -1,10 +1,28 @@
+import axios from 'axios';
+import { useEffect, useState, useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+  useEffect(() => {
+    axios
+      .get(
+        'https://react-native-project-7ac4f-default-rtdb.asia-southeast1.firebasedatabase.app/message.json?auth=' +
+          token,
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
